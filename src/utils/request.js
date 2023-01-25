8825151c744f79faa9e0a14a163394acfbb48089
sunlord <gj2011sd2011@163.com>
2023-01-25 23:11:51 +0800

    添加了mock使用方法

---
+++ src/utils/request.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const http = axios.create({
+    // 通用请求的地址前缀
+    baseURL: '/api',
+    timeout: 10000, //超时事件
+})
+
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+    // 2xx 范围内的状态码都会触发该函数。
+    // 对响应数据做点什么
+    
+    return response;
+  }, function (error) {
+    // 超出 2xx 范围的状态码都会触发该函数。
+    // 对响应错误做点什么
+    return Promise.reject(error);
+  });
+
+  export default http
